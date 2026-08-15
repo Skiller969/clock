9055f5bc9a2ee231aef3dbfed60bcb708f6e44f7
apple <sarthakpawar9308@gmail.com>
2026-08-15 06:52:26 +0530

    fisnished the basic clock ig i will add some new features now

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+// console.log("hello");
+
+const clock = document.querySelector(".clock");
+
+function updateClock() {
+  const now = new Date();
+
+  const options = {
+    timeZone: "Asia/Kolkata",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+  clock.textContent = now.toLocaleTimeString("en-US", options);
+}
+
+updateClock();
+setInterval(updateClock, 1000);
