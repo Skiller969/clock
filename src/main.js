c226ce610436b413a1c41b92e26c16e759ff6e3b
apple <sarthakpawar9308@gmail.com>
2026-08-16 21:26:46 +0530

    added feature from which u can select which countries time zone u wann see

--- src/main.js
+++ src/main.js
@@ -3,18 +3,46 @@ import "./style.css";
 // console.log("hello");
 
 const clock = document.querySelector(".clock");
+const prevTitle = document.querySelector(".Title");
+const btn = document.querySelector(".btn");
+const select = document.querySelector("#select");
+const countries = {
+  india: {
+    title: "Indian Standard Time",
+    timeZone: "Asia/Kolkata",
+  },
+
+  usa: {
+    title: "USA",
+    timeZone: "America/New_York",
+  },
+
+  japan: {
+    title: "Japan",
+    timeZone: "Asia/Tokyo",
+  },
+};
+
+let timeZone = "Asia/Kolkata";
 
 function updateClock() {
   const now = new Date();
 
   const options = {
-    timeZone: "Asia/Kolkata",
+    timeZone: timeZone,
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
   };
   clock.textContent = now.toLocaleTimeString("en-US", options);
 }
+btn.addEventListener("click", () => {
+  let selected_countries = countries[select.value];
+  prevTitle.textContent = selected_countries.title;
+  timeZone = selected_countries.timeZone;
+
+  updateClock();
+});
 
 updateClock();
 setInterval(updateClock, 1000);
